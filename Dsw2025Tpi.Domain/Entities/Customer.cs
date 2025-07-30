73014b240de52f343480687d7bbc9ebab0ebed3e
Josue Limo <josuelimomunoz@gmail.com>
2025-07-30 16:15:12 -0300

    Clases definidas

---
+++ Dsw2025Tpi.Domain/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    internal class Customer : EntityBase
+    {
+        public string? Email { get; set; }
+        public string? Name { get; set; }  
+        public string? PhoneNumber { get; set; }
+
+        public Customer(string? Email, string? Name, string? PhoneNumber)
+        {
+            this.Email = Email;
+            this.Name = Name;  
+            this.PhoneNumber = PhoneNumber;
+        }
+        public ICollection<Order>? Orders { get; set; } = new List<Order>();
+    }
+}
