f831ceeb0ce74dab12f87bf2f3c62bece285ffa8
Josue Limo <josuelimomunoz@gmail.com>
2025-08-02 01:41:10 -0300

    Empezando capa api, controlador de productos falta terminar

--- Dsw2025Tpi.Api/Program.cs
+++ Dsw2025Tpi.Api/Program.cs
@@ -1,8 +1,16 @@
-using Microsoft.EntityFrameworkCore;
-using Dsw2025Tpi.Data;
-using Dsw2025Tpi.Data.Helpers;
-using Microsoft.EntityFrameworkCore;
+using Dsw2025Tpi.Application.Interfaces;
+using Dsw2025Tpi.Application.Services;
+using Dsw2025Tpi.Application.Services;
 using Dsw2025Tpi.Data; // Este debe coincidir con el namespace donde están tus contextos
+using Dsw2025Tpi.Data.Helpers;
+using Dsw2025Tpi.Data.Repositories;
+using Dsw2025Tpi.Domain.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
+using System.Text;
 
 namespace Dsw2025Tpi.Api;
 
@@ -27,6 +35,36 @@ public class Program
         builder.Services.AddDbContext<AuthenticateContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("AuthDb")));
 
+
+        // Configuración de JWT PARA EL ENDPOINT 1
+        var jwtConfig = builder.Configuration.GetSection("Jwt");
+        var key = Encoding.UTF8.GetBytes(jwtConfig["Key"]);
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtConfig["Issuer"],
+                ValidAudience = jwtConfig["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                RoleClaimType = ClaimTypes.Role // Para que `[Authorize(Roles = "...")]` funcione
+            };
+        });
+
+        //builder.Services.AddAuthorization(); // Obligatorio para [Authorize]
+        //HASTA ESTO ES PARA FUNCIONAR EL ENDPOINT 1
+        builder.Services.AddScoped<IProductsManagementService, ProductsManagementService>();
+        builder.Services.AddScoped<IRepository, EfRepository>();
+
         var app = builder.Build();
 
         using (var scope = app.Services.CreateScope())
@@ -45,7 +83,11 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        //ENDPOINT 1
+        app.UseAuthentication();
+
         app.UseAuthorization();
+        // EDNPOINT 1
 
         app.MapControllers();
         
