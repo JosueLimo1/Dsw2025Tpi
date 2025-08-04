8d76f4b8bf502636f0263d1976b4a9416b70ea10
ignaciohillen <Ignacio.Hillen@alu.frt.utn.edu.ar>
2025-08-04 14:40:33 -0300

    Creacion de rama por errores en la anterior

--- Dsw2025Tpi.Domain/Entities/EntityBase.cs
+++ Dsw2025Tpi.Domain/Entities/EntityBase.cs
@@ -2,7 +2,7 @@
 
 public abstract class EntityBase
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } //Codigo unico alfanumerico
     protected EntityBase()
     {
         Id = Guid.NewGuid();
